cbc1e406c1e4382886fba814e8887b18c5ef9deb
Baruch Giverts <bor770@gmail.com>
2024-06-03 15:21:07 +0300

    mobile product

--- scripts/product.js
+++ scripts/product.js
@@ -1,29 +1,28 @@
-const CategoryApp = {
+const ProductApp = {
   async created() {
     const { categories, productData, svgIcons } = await init();
 
     this.categories = categories;
     this.svgIcons = svgIcons;
 
-    const selectedCategory = new URLSearchParams(location.search).get(
-      `category`
-    );
+    const selectedProduct = new URLSearchParams(location.search).get(`product`);
 
-    if (!categories.includes(selectedCategory)) {
+    if (!productData.map((product) => product.slug).includes(selectedProduct)) {
       location.replace(`/`);
     }
 
-    this.selectedCategory = selectedCategory;
-
-    this.selectedProducts = productData
-      .filter((product) => product.category === selectedCategory)
-      .toReversed();
+    this.selectedProduct = productData.find(
+      (product) => product.slug === selectedProduct
+    );
+    console.log(this.selectedProduct);
+    this.price = this.selectedProduct.price.toLocaleString();
   },
   data() {
     return {
       categories: [],
-      selectedCategory: null,
-      selectedProducts: [],
+      price: ``,
+      quantity: 1,
+      selectedProduct: {},
       svgIcons: {},
     };
   },
@@ -34,10 +33,10 @@ const CategoryApp = {
     categoryThumbnailImage(category) {
       return `/assets/shared/desktop/image-category-thumbnail-${category}.png`;
     },
-    productPageLink(product) {
+    otherPageLink(product) {
       return `/views/product.html?product=${product.slug}`;
     },
   },
 };
 
-Vue.createApp(CategoryApp).mount(`body`);
+Vue.createApp(ProductApp).mount(`body`);
